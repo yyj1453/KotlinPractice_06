eaf555bd0ba040969d891c364abf65872a48ab42
yyj14 <yyj1453@naver.com>
2023-04-26 15:24:05 +0900

    04/26 kotlin_practice_view

---
+++ app/src/main/java/org/techtown/kotlinpractice_06/MainActivity.kt
@@ -0,0 +1,16 @@
+package org.techtown.kotlinpractice_06
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView1 = findViewById<TextView>(R.id.text1)
+    }
+}
---
+++ viewbinging/src/main/java/org/techtown/viewbinging/MainActivity.kt
@@ -0,0 +1,23 @@
+package org.techtown.viewbinging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import org.techtown.viewbinging.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.visibleBtn.setOnClickListener {
+            binding.targetView.visibility = View.VISIBLE
+        }
+        binding.invisibleBtn.setOnClickListener {
+            binding.targetView.visibility = View.INVISIBLE
+        }
+    }
+}
